b821806d927d5617d7b4f7ab94a6a9a5a657294a
SV <wsukhov@gmail.com>
2022-06-24 17:47:12 +0300

    Initial Commit

---
+++ VKAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VKAppSwiftUI
+//
+//  Created by Cayenne on 24.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VKAppSwiftUI/VKAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKAppSwiftUIApp.swift
+//  VKAppSwiftUI
+//
+//  Created by Cayenne on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
