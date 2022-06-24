dce5281be3ae5f6237c26a0bb629f5569e7ae02f
SV <wsukhov@gmail.com>
2022-06-24 19:33:03 +0300

    first lesson

--- VKAppSwiftUI/ContentView.swift
+++ VKAppSwiftUI/ContentView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var login = ""
+    @State var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            
+            VStack {
+                LinearGradient(
+                    colors: [.red, .blue, .green, .yellow],
+                    startPoint: .leading,
+                    endPoint: .trailing
+                )
+                .mask  {
+                    Text("VK APP")
+                        .font(Font.system(size: 46, weight: .bold))
+                                .multilineTextAlignment(.center)
+                }
+                .frame(height: 100)
+                TextField("Логин", text: $login)
+                TextField("Пароль", text: $password)
+                Button("Войти") {
+                    logIn()
+                }
+                .frame(maxWidth: 200)
+                .buttonStyle(.borderedProminent)
+                
+            }
             .padding()
+            .textFieldStyle(.roundedBorder)
+        }
+    }
+    
+}
+
+extension ContentView {
+    func logIn() {
+        
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
